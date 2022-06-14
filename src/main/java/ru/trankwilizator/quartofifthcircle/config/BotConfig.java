32fb48ba270808d6a531311dc578a76d8e8988f6
Maxim1720 <musiev_maksim@mail.ru>
2022-06-14 12:34:29 +0400

    to remove unnecessary bins and add 'IBotCommand[] commands' bean.
    Исользуется классом Бота для регистрации команд

--- src/main/java/ru/trankwilizator/quartofifthcircle/config/BotConfig.java
+++ src/main/java/ru/trankwilizator/quartofifthcircle/config/BotConfig.java
@@ -1,37 +1,23 @@
 package ru.trankwilizator.quartofifthcircle.config;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
-import org.checkerframework.checker.units.qual.A;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.api.methods.updates.SetWebhook;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
-import ru.trankwilizator.quartofifthcircle.bot.Bot;
-import ru.trankwilizator.quartofifthcircle.bot.SpringTelegramBot;
+import org.telegram.telegrambots.extensions.bots.commandbot.commands.IBotCommand;
+import ru.trankwilizator.quartofifthcircle.command.NotesInScaleCommand;
+import ru.trankwilizator.quartofifthcircle.command.QuartoFifthCircleCommand;
+import ru.trankwilizator.quartofifthcircle.command.StartCommand;
 
 @Configuration
 @AllArgsConstructor
 public class BotConfig {
-
-    private final TelegramConfig telegramConfig;
     @Bean
-    public SetWebhook setWebhookInstance(){
-        return SetWebhook.builder().url(telegramConfig.getWebhookPath()).build();
-    }
-
-    @Bean
-    public SpringTelegramBot springTelegramBot(SetWebhook setWebhook){
-        SpringTelegramBot springTelegramBot = new SpringTelegramBot(setWebhook);
-
-        springTelegramBot.setBotPath(telegramConfig.getWebhookPath());
-        springTelegramBot.setBotToken(telegramConfig.getBotToken());
-        springTelegramBot.setBotUsername(telegramConfig.getBotName());
-
-        return springTelegramBot;
+    public IBotCommand[] commands(){
+        return new IBotCommand[]{
+                new StartCommand(),
+                new NotesInScaleCommand(),
+                new QuartoFifthCircleCommand()
+        };
     }
 
 
