455ea364531e4f92226f90565983c14b1b1a5365
Maxim1720 <musiev_maksim@mail.ru>
2022-06-15 23:16:55 +0400

    to add MessageHandlerException

---
+++ src/main/java/ru/trankwilizator/quartofifthcircle/exception/MessageHandlerException.java
@@ -0,0 +1,8 @@
+package ru.trankwilizator.quartofifthcircle.exception;
+
+public class MessageHandlerException extends RuntimeException{
+
+    public MessageHandlerException(String message){
+        super(message);
+    }
+}
