d1e7788d505be6cb07e77f909672b57ca9abc983
Maxim1720 <musiev_maksim@mail.ru>
2022-06-17 20:07:08 +0400

    for rename and refactor folders

--- src/main/java/ru/trankwilizator/quartofifthcircle/logic/scale/calculate/Scale.java
+++
@@ -1,6 +0,0 @@
-package ru.trankwilizator.quartofifthcircle.logic.scale.calculate;
-
-public interface Scale {
-    int NOTES_COUNT = 7;
-    int[] getPositions();
-}
---
+++ src/main/java/ru/trankwilizator/quartofifthcircle/logic/scale/calculate/step/ScalePositioner.java
@@ -0,0 +1,6 @@
+package ru.trankwilizator.quartofifthcircle.logic.scale.calculate.step;
+
+public interface ScalePositioner {
+    int NOTES_COUNT = 7;
+    int[] getPositions();
+}
