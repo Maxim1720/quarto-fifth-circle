acf2f4ec07e739daff151424208d067591cc22bf
Maxim1720 <musiev_maksim@mail.ru>
2022-06-17 20:06:50 +0400

    for rename and refactor folders

--- src/main/java/ru/trankwilizator/quartofifthcircle/logic/scale/calculate/util/ScaleCalculateStrategy.java
+++ src/main/java/ru/trankwilizator/quartofifthcircle/logic/scale/calculate/util/ScaleCalculateStrategy.java
@@ -1,11 +1,14 @@
-package ru.trankwilizator.quartofifthcircle.logic.scale.calculate;
+package ru.trankwilizator.quartofifthcircle.logic.scale.calculate.util;
 
 import ru.trankwilizator.quartofifthcircle.exception.ChordException;
 import ru.trankwilizator.quartofifthcircle.command.util.Fret;
+import ru.trankwilizator.quartofifthcircle.logic.scale.calculate.step.MajorScale;
+import ru.trankwilizator.quartofifthcircle.logic.scale.calculate.step.MinorScale;
+import ru.trankwilizator.quartofifthcircle.logic.scale.calculate.step.ScalePositioner;
 
 public class ScaleCalculateStrategy {
 
-    public static Scale getByFret(Fret chordFret){
+    public static ScalePositioner getByFret(Fret chordFret){
         switch (chordFret){
             case MINOR -> {
                 return new MinorScale();
