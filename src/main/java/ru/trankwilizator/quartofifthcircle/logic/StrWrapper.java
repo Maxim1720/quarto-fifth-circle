97263b8d85118b61f687568d6b045d9b47e0374f
Maxim1720 <musiev_maksim@mail.ru>
2022-06-17 19:59:34 +0400

    to define the base functionality in a super class

---
+++ src/main/java/ru/trankwilizator/quartofifthcircle/logic/StrWrapper.java
@@ -0,0 +1,34 @@
+package ru.trankwilizator.quartofifthcircle.logic;
+
+import org.slf4j.LoggerFactory;
+import ru.trankwilizator.quartofifthcircle.logic.chord.Chord;
+import ru.trankwilizator.quartofifthcircle.logic.chord.converter.ChordConverter;
+
+import java.util.Arrays;
+
+public abstract class StrWrapper<T> {
+    private final ChordConverter chordConverter;
+
+    protected StrWrapper(ChordConverter chordConverter) {
+        this.chordConverter = chordConverter;
+    }
+
+    public String[] getAsStrings(String keyChord) {
+        Chord tonicChord = chordConverter.convertToChord(keyChord);
+        T[] objects = getObjects(tonicChord);
+        String[] res = receiveStrs(objects);
+        LoggerFactory.getLogger(System.Logger.class).info(Arrays.toString(res) + " " + keyChord);
+        return res;
+    }
+
+    private String[] receiveStrs(T[] objects){
+        String[] strs = new String[objects.length];
+        for (int i = 0; i < objects.length; i++) {
+            strs[i] = objects[i].toString();
+        }
+        return strs;
+    }
+
+    protected abstract T[] getObjects(Chord tonicChord);
+
+}
