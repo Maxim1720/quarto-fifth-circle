c6431bb0e14057a7543a079fb679a00267b0803f
Maxim1720 <musiev_maksim@mail.ru>
2022-06-17 19:43:52 +0400

    to set Component

--- src/main/java/ru/trankwilizator/quartofifthcircle/command/NotCommand.java
+++ src/main/java/ru/trankwilizator/quartofifthcircle/command/NotCommand.java
@@ -1,5 +1,6 @@
 package ru.trankwilizator.quartofifthcircle.command;
 
+import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.extensions.bots.commandbot.commands.IBotCommand;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
@@ -8,6 +9,7 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.util.function.BiConsumer;
 
+@Component
 public class NotCommand implements BiConsumer<AbsSender, Message> {
 
     @Override
