45eee5e51c39137065806369974dd904bc51f5ac
Maxim1720 <musiev_maksim@mail.ru>
2022-06-17 20:07:04 +0400

    for rename and refactor folders

---
+++ src/main/java/ru/trankwilizator/quartofifthcircle/logic/scale/calculate/step/MinorScale.java
@@ -0,0 +1,11 @@
+package ru.trankwilizator.quartofifthcircle.logic.scale.calculate.step;
+
+public class MinorScale implements ScalePositioner {
+
+    @Override
+    public int[] getPositions() {
+        return new int[]{
+                2,1,2,2,1,2
+        };
+    }
+}
