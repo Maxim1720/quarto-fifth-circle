df9aaf6bccbbb88d81ec0c15091dd6f9db37047a
Maxim1720 <musiev_maksim@mail.ru>
2022-06-17 20:03:52 +0400

    for dependency injection.

--- src/main/java/ru/trankwilizator/quartofifthcircle/logic/scale/calculate/ScaleCalculator.java
+++ src/main/java/ru/trankwilizator/quartofifthcircle/logic/scale/calculate/ScaleCalculator.java
@@ -1,17 +1,19 @@
-package ru.trankwilizator.quartofifthcircle.logic.scale;
+package ru.trankwilizator.quartofifthcircle.logic.scale.calculate;
 
+import org.springframework.stereotype.Component;
 import ru.trankwilizator.quartofifthcircle.command.util.NotePositionCorrector;
 import ru.trankwilizator.quartofifthcircle.command.util.Notes;
 import ru.trankwilizator.quartofifthcircle.logic.chord.Chord;
-import ru.trankwilizator.quartofifthcircle.logic.scale.calculate.Scale;
-import ru.trankwilizator.quartofifthcircle.logic.scale.calculate.ScaleCalculateStrategy;
+import ru.trankwilizator.quartofifthcircle.logic.scale.calculate.step.ScalePositioner;
+import ru.trankwilizator.quartofifthcircle.logic.scale.calculate.util.ScaleCalculateStrategy;
 
-public class ScaleCalculator {
+@Component
+public class ScaleCalculator implements IScaleCalculator {
     public Notes[] calculate(Chord tonicChord) {
-        Notes[] notes = new Notes[Scale.NOTES_COUNT];
+        Notes[] notes = new Notes[ScalePositioner.NOTES_COUNT];
         int[] steps = ScaleCalculateStrategy.getByFret(tonicChord.getFret()).getPositions();
         notes[0] = tonicChord.getKeyNote();
-        for (int i = 1; i < Scale.NOTES_COUNT; i++) {
+        for (int i = 1; i < ScalePositioner.NOTES_COUNT; i++) {
             notes[i] = Notes.values()[NotePositionCorrector.correct(notes[i - 1].ordinal() + steps[i-1])];
         }
         return notes;
