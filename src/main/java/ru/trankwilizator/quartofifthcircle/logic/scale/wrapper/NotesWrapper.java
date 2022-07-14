7e9983b9a999a0747926aba78dd5d4af8eab9957
Maxim1720 <musiev_maksim@mail.ru>
2022-07-14 08:20:59 +0400

    to add Qualifier for NotesWrapper

--- src/main/java/ru/trankwilizator/quartofifthcircle/logic/scale/wrapper/NotesWrapper.java
+++ src/main/java/ru/trankwilizator/quartofifthcircle/logic/scale/wrapper/NotesWrapper.java
@@ -1,13 +1,16 @@
-package ru.trankwilizator.quartofifthcircle.logic.scale;
+package ru.trankwilizator.quartofifthcircle.logic.scale.wrapper;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import ru.trankwilizator.quartofifthcircle.command.util.Notes;
 import ru.trankwilizator.quartofifthcircle.logic.StrWrapper;
 import ru.trankwilizator.quartofifthcircle.logic.chord.Chord;
 import ru.trankwilizator.quartofifthcircle.logic.chord.converter.ChordConverter;
+import ru.trankwilizator.quartofifthcircle.logic.scale.IScale;
 
 @Component
+@Qualifier("notesWrapper")
 public class NotesWrapper extends StrWrapper<Notes> {
 
     private final IScale scale;
