9c10c52520e3b8b2ebe9f93be4e74f89f2b31644
Maxim1720 <musiev_maksim@mail.ru>
2022-07-14 08:38:32 +0400

    to refactor

--- src/main/java/ru/trankwilizator/quartofifthcircle/command/Command.java
+++ src/main/java/ru/trankwilizator/quartofifthcircle/command/Command.java
@@ -1,13 +1,12 @@
 package ru.trankwilizator.quartofifthcircle.command;
 
 import org.springframework.context.support.MessageSourceResourceBundle;
-import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.extensions.bots.commandbot.commands.IBotCommand;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.bots.AbsSender;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import ru.trankwilizator.quartofifthcircle.command.strings.StrAnswerBuilder;
+import ru.trankwilizator.quartofifthcircle.util.StrAnswerBuilder;
 import ru.trankwilizator.quartofifthcircle.exception.ChordException;
 import ru.trankwilizator.quartofifthcircle.logic.StrWrapper;
 
