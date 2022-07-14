e4a252d1212165a2ddde570e105e651bcab3154e
Maxim1720 <musiev_maksim@mail.ru>
2022-07-14 08:16:55 +0400

    to remove unnecessary classes

--- src/main/java/ru/trankwilizator/quartofifthcircle/bot/message/BotMessagesHandler.java
+++
@@ -1,9 +0,0 @@
-package ru.trankwilizator.quartofifthcircle.bot.message;
-
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.Message;
-import ru.trankwilizator.quartofifthcircle.exception.MessageHandlerException;
-
-public interface BotMessagesHandler {
-    SendMessage getAnswer(Message message) throws MessageHandlerException;
-}
