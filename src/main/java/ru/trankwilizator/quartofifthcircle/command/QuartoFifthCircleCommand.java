cee17dbc2e9ab0479986527cd4428fc0f66d2ab6
Maxim1720 <musiev_maksim@mail.ru>
2022-07-14 08:38:50 +0400

    to refactor

--- src/main/java/ru/trankwilizator/quartofifthcircle/command/QuartoFifthCircleCommand.java
+++ src/main/java/ru/trankwilizator/quartofifthcircle/command/QuartoFifthCircleCommand.java
@@ -3,13 +3,9 @@ package ru.trankwilizator.quartofifthcircle.command;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.MessageSourceResourceBundle;
 import org.springframework.stereotype.Component;
-import org.telegram.telegrambots.extensions.bots.commandbot.commands.IBotCommand;
-import org.telegram.telegrambots.meta.api.objects.Message;
-import org.telegram.telegrambots.meta.bots.AbsSender;
-import ru.trankwilizator.quartofifthcircle.command.strings.StrAnswerBuilder;
+import ru.trankwilizator.quartofifthcircle.util.StrAnswerBuilder;
 import ru.trankwilizator.quartofifthcircle.logic.StrWrapper;
 import ru.trankwilizator.quartofifthcircle.logic.chord.Chord;
-import ru.trankwilizator.quartofifthcircle.logic.quarto_fifth_circle.QuartoFifthCircle;
 
 @Component
 public class QuartoFifthCircleCommand extends Command<Chord> {
