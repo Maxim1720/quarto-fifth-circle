70f796a9c425f0c1bba0d12e311a13a8ef191549
Maxim1720 <musiev_maksim@mail.ru>
2022-07-14 08:16:46 +0400

    to remove unnecessary statements

--- src/main/java/ru/trankwilizator/quartofifthcircle/bot/Bot.java
+++ src/main/java/ru/trankwilizator/quartofifthcircle/bot/Bot.java
@@ -7,19 +7,14 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.extensions.bots.commandbot.TelegramLongPollingCommandBot;
 import org.telegram.telegrambots.extensions.bots.commandbot.commands.IBotCommand;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.Chat;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import ru.trankwilizator.quartofifthcircle.bot.log.MessagesLogToTrankwilizator;
 import ru.trankwilizator.quartofifthcircle.bot.log.MessagesLogger;
-import ru.trankwilizator.quartofifthcircle.bot.message.BotMessagesHandler;
 import ru.trankwilizator.quartofifthcircle.command.NotCommand;
 
 @Component
 public class Bot extends TelegramLongPollingCommandBot {
-    private final BotMessagesHandler botMessageHandler;
 
     @Value("${telegram.bot.username}")
     private String BOT_NAME;
@@ -28,9 +23,7 @@ public class Bot extends TelegramLongPollingCommandBot {
     private String BOT_TOKEN;
 
     @Autowired
-    public Bot(BotMessagesHandler botMessageHandler,
-               IBotCommand[] commands){
-        this.botMessageHandler = botMessageHandler;
+    public Bot(IBotCommand[] commands){
         registerAll(commands);
         registerDefaultAction(new NotCommand());
     }
