73ab019306a955839b04332376407f126e71046a
Maxim1720 <musiev_maksim@mail.ru>
2022-07-14 08:20:23 +0400

    to refactor logic

--- src/main/java/ru/trankwilizator/quartofifthcircle/logic/quarto_fifth_circle/QuartoFifthCircle.java
+++ src/main/java/ru/trankwilizator/quartofifthcircle/logic/quarto_fifth_circle/QuartoFifthCircle.java
@@ -1,15 +1,46 @@
 package ru.trankwilizator.quartofifthcircle.logic.quarto_fifth_circle;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import ru.trankwilizator.quartofifthcircle.command.util.Fret;
+import ru.trankwilizator.quartofifthcircle.command.util.Notes;
 import ru.trankwilizator.quartofifthcircle.logic.chord.Chord;
-import ru.trankwilizator.quartofifthcircle.logic.quarto_fifth_circle.chordCalculator.ChordCalculator;
-import ru.trankwilizator.quartofifthcircle.logic.quarto_fifth_circle.chordCalculator.ChordCalculatorStrategy;
+import ru.trankwilizator.quartofifthcircle.logic.scale.IScale;
+import ru.trankwilizator.quartofifthcircle.logic.scale.calculate.step.ScalePositioner;
 
-public class QuartoFifthCircle {
+@Service
+@Qualifier("quartoFifthCircle")
+public class QuartoFifthCircle implements IQuartoFifthCircle {
 
-    public Chord[] getChords(Chord chord) {
-        int keyNotePosition = chord.getKeyNote().ordinal();
-        ChordCalculator chordCalculator = ChordCalculatorStrategy.INSTANCE(chord.getFret());
-        return chordCalculator.calculateChords(keyNotePosition);
+    private final IScale scale;
+
+    public QuartoFifthCircle(IScale scale) {
+        this.scale = scale;
     }
 
+    @Override
+    public Chord[] getChords(Chord chord) {
+        Notes[] notes = scale.getNotes(chord);
+        return makeChords(notes, chord);
+    }
+
+    private Chord[] makeChords(Notes[] notes, Chord tonic) {
+
+        Chord[] chords = new Chord[ScalePositioner.NOTES_COUNT - 1];
+        chords[0] = tonic;
+        if (tonic.getFret().equals(Fret.MINOR)) {
+            chords[1] = new Chord(notes[2], Fret.MAJOR);
+            chords[2] = new Chord(notes[3], Fret.MINOR);
+            chords[3] = new Chord(notes[4], Fret.MINOR);
+            chords[4] = new Chord(notes[5], Fret.MAJOR);
+            chords[5] = new Chord(notes[6], Fret.MAJOR);
+        } else {
+            chords[1] = new Chord(notes[1], Fret.MINOR);
+            chords[2] = new Chord(notes[2], Fret.MINOR);
+            chords[3] = new Chord(notes[3], Fret.MAJOR);
+            chords[4] = new Chord(notes[4], Fret.MAJOR);
+            chords[5] = new Chord(notes[5], Fret.MINOR);
+        }
+        return chords;
+    }
 }
