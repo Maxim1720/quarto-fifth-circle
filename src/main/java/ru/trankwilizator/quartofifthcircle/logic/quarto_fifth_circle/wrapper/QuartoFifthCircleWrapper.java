ab8d9b3ba7636a182543e7114137d34781a44374
Maxim1720 <musiev_maksim@mail.ru>
2022-07-14 08:19:53 +0400

    to add QuartoFifthCircleWrapper

---
+++ src/main/java/ru/trankwilizator/quartofifthcircle/logic/quarto_fifth_circle/wrapper/QuartoFifthCircleWrapper.java
@@ -0,0 +1,28 @@
+package ru.trankwilizator.quartofifthcircle.logic.quarto_fifth_circle.wrapper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+import ru.trankwilizator.quartofifthcircle.logic.StrWrapper;
+import ru.trankwilizator.quartofifthcircle.logic.chord.Chord;
+import ru.trankwilizator.quartofifthcircle.logic.chord.converter.ChordConverter;
+import ru.trankwilizator.quartofifthcircle.logic.quarto_fifth_circle.IQuartoFifthCircle;
+
+@Component
+public class QuartoFifthCircleWrapper extends StrWrapper<Chord> {
+
+    private final IQuartoFifthCircle quartoFifthCircle2;
+
+    @Autowired
+    protected QuartoFifthCircleWrapper(ChordConverter chordConverter,
+                                       @Qualifier("quartoFifthCircle") IQuartoFifthCircle quartoFifthCircle2) {
+        super(chordConverter);
+        this.quartoFifthCircle2 = quartoFifthCircle2;
+    }
+
+    @Override
+    protected Chord[] getObjects(Chord tonicChord) {
+        return quartoFifthCircle2.getChords(tonicChord);
+    }
+
+}
