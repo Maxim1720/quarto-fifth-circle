414a4c6c3b069fdd75e9e7bfacbd3677e1dfc4e1
Maxim1720 <musiev_maksim@mail.ru>
2022-07-14 08:19:14 +0400

    to add PentatonicWrapper

---
+++ src/main/java/ru/trankwilizator/quartofifthcircle/logic/pentatonic/wrapper/PentatonicWrapper.java
@@ -0,0 +1,27 @@
+package ru.trankwilizator.quartofifthcircle.logic.pentatonic.wrapper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+import ru.trankwilizator.quartofifthcircle.command.util.Notes;
+import ru.trankwilizator.quartofifthcircle.logic.StrWrapper;
+import ru.trankwilizator.quartofifthcircle.logic.chord.Chord;
+import ru.trankwilizator.quartofifthcircle.logic.chord.converter.ChordConverter;
+import ru.trankwilizator.quartofifthcircle.logic.pentatonic.Pentatonic;
+
+@Component
+@Qualifier("pentatonicWrapper")
+public class PentatonicWrapper extends StrWrapper<Notes> {
+
+    private final Pentatonic pentatonic;
+
+    @Autowired
+    protected PentatonicWrapper(ChordConverter chordConverter, Pentatonic pentatonic) {
+        super(chordConverter);
+        this.pentatonic = pentatonic;
+    }
+    @Override
+    protected Notes[] getObjects(Chord tonicChord) {
+        return pentatonic.getNotes(tonicChord);
+    }
+}
