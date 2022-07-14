559df0a628d935eb68afacfe79aad71483a2f6bf
Maxim1720 <musiev_maksim@mail.ru>
2022-07-14 08:18:28 +0400

    to reformat logger

--- src/main/java/ru/trankwilizator/quartofifthcircle/bot/log/MessagesLogger.java
+++ src/main/java/ru/trankwilizator/quartofifthcircle/bot/log/MessagesLogger.java
@@ -1,26 +1,44 @@
 package ru.trankwilizator.quartofifthcircle.bot.log;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
 public class MessagesLogger {
 
-    private static final Logger logger = LoggerFactory.getLogger(Logger.class);
+    private static final Logger logger;
 
-    public static void logMessageRecivied(User user){
-        logger.info("message received from " + user.getFirstName() + " " + user.getLastName() + ": username is '" + user.getUserName() + "'"
-        + " id: " + user.getId());
+    static {
+        logger = Logger.getLogger(System.Logger.Level.INFO.getName());
+        try {
+            FileHandler fileHandler = new FileHandler("src/main/resources/logs.txt", true);
+            logger.addHandler(fileHandler);
+            SimpleFormatter formatter = new SimpleFormatter();
+            fileHandler.setFormatter(formatter);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void logMessageReceived(User user, Message message){
+        String from = "message received from:\n";
+        String userInfo = user.getFirstName() + " " + user.getLastName() + "\n" + user.getUserName();
+        logger.info(from +  userInfo + "\nid: " + user.getId());
+        logger.info("received: " + message.getText());
     }
 
     public static void logMessageSent(SendMessage sendMessage){
-        logger.info("sent message: " + sendMessage.getText());
+        logger.info("sent message: " + sendMessage.getText().replace('\n',' '));
     }
 
-    public static void logError(TelegramApiException e){
-        logger.error(e.getMessage(), e);
+    public static void logError(Class clazz,TelegramApiException e){
+        logger.throwing(clazz.getName(), e.getMessage(), e);
     }
 
 }
