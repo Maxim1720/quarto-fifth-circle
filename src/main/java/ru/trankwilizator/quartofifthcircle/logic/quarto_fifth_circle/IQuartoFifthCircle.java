2ab782ee52b4d91d278b806ac1c031bcb5574a8f
Maxim1720 <musiev_maksim@mail.ru>
2022-07-14 08:20:07 +0400

    to rename method

---
+++ src/main/java/ru/trankwilizator/quartofifthcircle/logic/quarto_fifth_circle/IQuartoFifthCircle.java
@@ -0,0 +1,7 @@
+package ru.trankwilizator.quartofifthcircle.logic.quarto_fifth_circle;
+
+import ru.trankwilizator.quartofifthcircle.logic.chord.Chord;
+
+public interface IQuartoFifthCircle {
+    Chord[] getChords(Chord chord);
+}
--- src/main/java/ru/trankwilizator/quartofifthcircle/logic/quarto_fifth_circle/chordCalculator/ChordCalculator.java
+++
@@ -1,7 +0,0 @@
-package ru.trankwilizator.quartofifthcircle.logic.quarto_fifth_circle.chordCalculator;
-
-import ru.trankwilizator.quartofifthcircle.logic.chord.Chord;
-
-public interface ChordCalculator {
-    Chord[] calculateChords(int keyPosition);
-}
