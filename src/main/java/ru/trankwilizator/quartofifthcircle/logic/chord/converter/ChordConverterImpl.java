2ec3fd9a8a8b13fc7c18a87c6c2f35a58b5dd923
Maxim1720 <musiev_maksim@mail.ru>
2022-07-14 08:38:57 +0400

    to refactor

--- src/main/java/ru/trankwilizator/quartofifthcircle/logic/chord/converter/ChordConverterImpl.java
+++ src/main/java/ru/trankwilizator/quartofifthcircle/logic/chord/converter/ChordConverterImpl.java
@@ -1,11 +1,9 @@
 package ru.trankwilizator.quartofifthcircle.logic.chord.converter;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import ru.trankwilizator.quartofifthcircle.command.util.Fret;
-import ru.trankwilizator.quartofifthcircle.command.util.Notes;
+import ru.trankwilizator.quartofifthcircle.util.Fret;
+import ru.trankwilizator.quartofifthcircle.util.Notes;
 import ru.trankwilizator.quartofifthcircle.logic.chord.Chord;
 import ru.trankwilizator.quartofifthcircle.logic.chord.validator.ChordValidator;
 
