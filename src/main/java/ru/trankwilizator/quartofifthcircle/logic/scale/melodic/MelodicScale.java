71d79b2bdcd41868876a8f0df67dce77c48ada89
Maxim1720 <musiev_maksim@mail.ru>
2022-07-17 04:22:03 +0400

    to add naturalScaleCalculator for get natural scale notes in melodic

--- src/main/java/ru/trankwilizator/quartofifthcircle/logic/scale/melodic/MelodicScale.java
+++ src/main/java/ru/trankwilizator/quartofifthcircle/logic/scale/melodic/MelodicScale.java
@@ -4,8 +4,16 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import ru.trankwilizator.quartofifthcircle.logic.chord.Chord;
 import ru.trankwilizator.quartofifthcircle.logic.scale.Scale;
+import ru.trankwilizator.quartofifthcircle.logic.scale.calculator.ScaleCalculator;
 import ru.trankwilizator.quartofifthcircle.logic.scale.calculator.melodic.MelodicScaleCalculator;
+import ru.trankwilizator.quartofifthcircle.logic.scale.calculator.natural.NaturalScaleCalculator;
+import ru.trankwilizator.quartofifthcircle.util.Fret;
 import ru.trankwilizator.quartofifthcircle.util.Notes;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @Component
 @Qualifier("melodicScale")
@@ -17,11 +25,33 @@ public class MelodicScale implements Scale {
     public MelodicScale(MelodicScaleCalculator melodicScaleCalculator,
                         NaturalScaleCalculator naturalScaleCalculator) {
         this.melodicScaleCalculator = melodicScaleCalculator;
+        this.naturalScaleCalculator = naturalScaleCalculator;
     }
 
     @Override
     public Notes[] getNotes(Chord chord) {
-        return melodicScaleCalculator.calculate(chord);
+
+        ScaleCalculator scaleCalculator1;
+        ScaleCalculator scaleCalculator2;
+
+        if(chord.getFret().equals(Fret.MINOR)){
+            scaleCalculator1 = melodicScaleCalculator;
+            scaleCalculator2 = naturalScaleCalculator;
+        }
+        else {
+            scaleCalculator1 = naturalScaleCalculator;
+            scaleCalculator2 = melodicScaleCalculator;
+        }
+
+        ArrayList<Notes> notes = new ArrayList<>(Arrays.stream(scaleCalculator1.calculate(chord)).toList());
+
+        List<Notes> notesList = new ArrayList<>(Arrays.stream(scaleCalculator2.calculate(chord)).toList());
+
+        Collections.reverse(notesList);
+
+        notes.addAll(notesList);
+
+        return toNotesArray(notes);
     }
 
     private Notes[] toNotesArray(List<Notes> notes){
