ea0e8a32710dc923429e5859b247b5923bec9be3
Maxim1720 <musiev_maksim@mail.ru>
2022-07-18 23:32:15 +0400

    to remove unnecessary imports

--- src/main/java/ru/trankwilizator/quartofifthcircle/bot/Bot.java
+++ src/main/java/ru/trankwilizator/quartofifthcircle/bot/Bot.java
@@ -1,7 +1,6 @@
 package ru.trankwilizator.quartofifthcircle.bot;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.support.MessageSourceResourceBundle;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.extensions.bots.commandbot.TelegramLongPollingCommandBot;
