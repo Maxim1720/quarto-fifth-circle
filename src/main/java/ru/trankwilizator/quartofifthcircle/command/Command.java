7bb3d272190d3adc1ffcb26a1c9f2a4f9ebb43b5
Maxim1720 <musiev_maksim@mail.ru>
2022-07-18 23:33:46 +0400

    to delegate command logic to CommandMessageProcessor

--- src/main/java/ru/trankwilizator/quartofifthcircle/command/Command.java
+++ src/main/java/ru/trankwilizator/quartofifthcircle/command/Command.java
@@ -5,69 +5,13 @@ import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.bots.AbsSender;
 import ru.trankwilizator.quartofifthcircle.message.MessageProcessor;
 
-public abstract class Command<T> implements IBotCommand {
-    private final StrWrapper<T> wrapper;
-    private final StrAnswerBuilder strAnswerBuilder;
-
-    protected Command(StrWrapper<T> wrapper,
-                      StrAnswerBuilder strAnswerBuilder) {
-        this.wrapper = wrapper;
-        this.strAnswerBuilder = strAnswerBuilder;
+public abstract class Command implements IBotCommand {
+    private final MessageProcessor messageProcessor;
+    protected Command(MessageProcessor messageProcessor) {
+        this.messageProcessor = messageProcessor;
     }
-
     @Override
     public void processMessage(AbsSender absSender, Message message, String[] strings) {
-        SendMessage sendMessage;
-        try {
-            checkIsEmptyStrings(strings);
-            sendAnswers(strings,message,absSender);
-        }
-        catch (ChordException e){
-            sendMessage = new SendMessage(message.getChat().getId().toString(),
-                    e.getMessage());
-            tryExecuteSendMessage(absSender,sendMessage);
-        }
+        messageProcessor.processMessage(absSender,message,strings);
     }
-
-    private void sendAnswers(String[] strings, Message message, AbsSender sender){
-        for (String chordText :
-                strings) {
-            SendMessage sendMessage;
-            try {
-                sendMessage = new SendMessage(message.getChat().getId().toString(),
-                        strAnswerBuilder.build(getResultStrings(chordText)));
-            }
-            catch (ChordException e){
-                sendMessage = new SendMessage(message.getChat().getId().toString(),
-                        e.getMessage());
-            }
-            tryExecuteSendMessage(sender,sendMessage);
-        }
-    }
-
-    protected StrWrapper<T> wrapper(){
-        return wrapper;
-    }
-
-    protected String[] getResultStrings(String tonality){
-        return wrapper().getAsStrings(tonality);
-    }
-
-    private void checkIsEmptyStrings(String[] strings){
-        if(strings.length == 0){
-            throw new ChordException(MessageSourceResourceBundle
-                    .getBundle("messages")
-                    .getString("bot.chord.error.empty"));
-        }
-    }
-    private void tryExecuteSendMessage(AbsSender sender, SendMessage sendMessage){
-        try {
-            sender.execute(sendMessage);
-        }
-        catch (TelegramApiException e){
-            throw new RuntimeException();
-        }
-    }
-
-
 }
