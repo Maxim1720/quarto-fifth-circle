5eb0a4ac0fa761b2d3be5989f56d753e5731e6c9
Maxim1720 <musiev_maksim@mail.ru>
2022-07-18 23:32:47 +0400

    to remove unnecessary imports

--- src/main/java/ru/trankwilizator/quartofifthcircle/command/Command.java
+++ src/main/java/ru/trankwilizator/quartofifthcircle/command/Command.java
@@ -1,8 +1,6 @@
 package ru.trankwilizator.quartofifthcircle.command;
 
-import org.springframework.context.support.MessageSourceResourceBundle;
 import org.telegram.telegrambots.extensions.bots.commandbot.commands.IBotCommand;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.bots.AbsSender;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
