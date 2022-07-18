654da10f549c0abd9300fc20f67d3fcb568e0989
Maxim1720 <musiev_maksim@mail.ru>
2022-07-18 23:36:21 +0400

    to add HarmonicScaleCalculator

---
+++ src/main/java/ru/trankwilizator/quartofifthcircle/logic/scale/calculator/harmonic/HarmonicScaleCalculator.java
@@ -0,0 +1,38 @@
+package ru.trankwilizator.quartofifthcircle.logic.scale.calculator.harmonic;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import ru.trankwilizator.quartofifthcircle.logic.chord.Chord;
+import ru.trankwilizator.quartofifthcircle.logic.scale.calculator.ScaleCalculator;
+import ru.trankwilizator.quartofifthcircle.logic.scale.calculator.natural.NaturalScaleCalculator;
+import ru.trankwilizator.quartofifthcircle.util.Fret;
+import ru.trankwilizator.quartofifthcircle.util.NotePositionCorrector;
+import ru.trankwilizator.quartofifthcircle.util.Notes;
+
+@Component
+public class HarmonicScaleCalculator implements ScaleCalculator {
+
+    private final NaturalScaleCalculator naturalScaleCalculator;
+
+    @Autowired
+    public HarmonicScaleCalculator(NaturalScaleCalculator naturalScaleCalculator) {
+        this.naturalScaleCalculator = naturalScaleCalculator;
+    }
+
+    @Override
+    public Notes[] calculate(Chord tonicChord) {
+        Notes[] notes = naturalScaleCalculator.calculate(tonicChord);
+
+        if(tonicChord.getFret().equals(Fret.MINOR)){
+            notes[6] = getFrom(notes[6],1);
+        }
+        else {
+            notes[5] = getFrom(notes[5],-1);
+        }
+        return notes;
+    }
+
+    private Notes getFrom(Notes note, int i){
+        return Notes.values()[NotePositionCorrector.correct(note.ordinal() + i)];
+    }
+}
