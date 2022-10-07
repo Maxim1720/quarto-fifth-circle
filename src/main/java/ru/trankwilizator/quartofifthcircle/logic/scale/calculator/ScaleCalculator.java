6075868e1865ca106b4ca2cd5b33a32437994c3d
maksim1708 <musiev_maksim@mail.ru>
2022-10-07 19:42:29 +0400

    to rename packages and classes from 'Chord..' to 'Tonality..'

--- src/main/java/ru/trankwilizator/quartofifthcircle/logic/scale/calculator/ScaleCalculator.java
+++ src/main/java/ru/trankwilizator/quartofifthcircle/logic/scale/calculator/ScaleCalculator.java
@@ -2,9 +2,8 @@ package ru.trankwilizator.quartofifthcircle.logic.scale.calculator;
 
 
 import ru.trankwilizator.quartofifthcircle.util.Notes;
-import ru.trankwilizator.quartofifthcircle.logic.chord.Chord;
+import ru.trankwilizator.quartofifthcircle.logic.tonality.Tonality;
 
 public interface ScaleCalculator {
-
-    Notes[] calculate(Chord tonicChord);
+    Notes[] calculate(Tonality tonality);
 }
