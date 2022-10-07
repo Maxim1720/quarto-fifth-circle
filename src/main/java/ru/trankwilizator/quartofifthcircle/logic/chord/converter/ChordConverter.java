5c5b87e817894e2c13b15ab82b3aac2449a4d95e
maksim1708 <musiev_maksim@mail.ru>
2022-10-07 19:25:20 +0400

    to rename packages and classes from 'Chord..' to 'Tonality..'

--- src/main/java/ru/trankwilizator/quartofifthcircle/logic/chord/converter/ChordConverter.java
+++
@@ -1,8 +0,0 @@
-package ru.trankwilizator.quartofifthcircle.logic.chord.converter;
-
-import ru.trankwilizator.quartofifthcircle.logic.chord.Chord;
-
-public interface ChordConverter {
-    Chord convertToChord(String chord);
-
-}
--- src/main/java/ru/trankwilizator/quartofifthcircle/logic/chord/validator/ChordValidator.java
+++
@@ -1,7 +0,0 @@
-package ru.trankwilizator.quartofifthcircle.logic.chord.validator;
-
-import ru.trankwilizator.quartofifthcircle.exception.ChordException;
-
-public interface ChordValidator {
-    void validate(String chord) throws ChordException;
-}
