d766c956348c0a20fad127295e7861c768a1d959
maksim1708 <musiev_maksim@mail.ru>
2023-12-03 23:31:26 +0500

    to refactor

--- src/main/java/ru/trankwilizator/quartofifthcircle/logic/scale/calculator/natural/util/ScaleCalculateStrategy.java
+++ src/main/java/ru/trankwilizator/quartofifthcircle/logic/scale/calculator/natural/util/ScaleCalculateStrategy.java
@@ -1,6 +1,6 @@
 package ru.trankwilizator.quartofifthcircle.logic.scale.calculator.natural.util;
 
-import ru.trankwilizator.quartofifthcircle.exception.ChordException;
+import ru.trankwilizator.quartofifthcircle.exception.TonalityException;
 import ru.trankwilizator.quartofifthcircle.util.Fret;
 import ru.trankwilizator.quartofifthcircle.logic.scale.calculator.natural.step.MajorScale;
 import ru.trankwilizator.quartofifthcircle.logic.scale.calculator.natural.step.MinorScale;
@@ -10,13 +10,11 @@ public class ScaleCalculateStrategy {
 
     public static ScalePositioner getByFret(Fret chordFret){
         switch (chordFret){
-            case MINOR -> {
+            case MINOR:
                 return new MinorScale();
-            }
-            case MAJOR -> {
+            case MAJOR:
                 return new MajorScale();
-            }
-            default -> throw new ChordException("Fret Not Found");
+            default: throw new TonalityException("Fret Not Found");
         }
     }
 
