41b850859dd5188fce1f87c3e0e12ac12f56480c
maksim1708 <musiev_maksim@mail.ru>
2023-12-03 23:26:21 +0500

    to rename and mv

--- src/main/java/ru/trankwilizator/quartofifthcircle/bot/command/BotCommand.java
+++ src/main/java/ru/trankwilizator/quartofifthcircle/bot/command/BotCommand.java
@@ -1,13 +1,13 @@
-package ru.trankwilizator.quartofifthcircle.command;
+package ru.trankwilizator.quartofifthcircle.bot.command;
 
 import org.telegram.telegrambots.extensions.bots.commandbot.commands.IBotCommand;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.bots.AbsSender;
-import ru.trankwilizator.quartofifthcircle.message.MessageProcessor;
+import ru.trankwilizator.quartofifthcircle.message.processor.MessageProcessor;
 
-public abstract class Command implements IBotCommand {
+public abstract class BotCommand implements IBotCommand {
     private final MessageProcessor messageProcessor;
-    protected Command(MessageProcessor messageProcessor) {
+    protected BotCommand(MessageProcessor messageProcessor) {
         this.messageProcessor = messageProcessor;
     }
     @Override
